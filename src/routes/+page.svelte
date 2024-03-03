53c60aee2ea11a4cf2268e0f4bd39c6eacaa5245
CVakratsas <c.vakratsas@gmail.com>
2024-03-03 14:35:07 +0200

    Initialized sveltekit project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    let title = 'Ninja Gaming Guides';
+</script>
+
+<div class="index">
+    <h1>{title}</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque qui eligendi distinctio libero voluptate repellendus incidunt ipsum debitis. Eum ex aliquam iure fugiat dolorem unde deleniti sint quam earum eius corrupti harum beatae nemo at, repudiandae ipsam dolore quibusdam eaque voluptatem distinctio maiores itaque quisquam quis. Aut quo eveniet at ipsum ipsam quasi officia rem! Molestias eligendi voluptatibus nihil magnam!</p>
+</div>
+
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;    
+    }
+</style>

36a8aed37cf093928df7d5c6fadf3a5b768aa1ec
CVakratsas <c.vakratsas@gmail.com>
2024-03-05 18:08:08 +0200

    All routes complete

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script>
-    let title = 'Ninja Gaming Guides';
+    
 </script>
 
 <div class="index">
-    <h1>{title}</h1>
+    <h1>Welcome</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque qui eligendi distinctio libero voluptate repellendus incidunt ipsum debitis. Eum ex aliquam iure fugiat dolorem unde deleniti sint quam earum eius corrupti harum beatae nemo at, repudiandae ipsam dolore quibusdam eaque voluptatem distinctio maiores itaque quisquam quis. Aut quo eveniet at ipsum ipsam quasi officia rem! Molestias eligendi voluptatibus nihil magnam!</p>
+    <a href="/guides">View Guides</a>
+    <a href="/about">About</a>
 </div>
 
 <style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+   
+</script>
+
+<div class="about">
+    <h2>About Us</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae nemo fuga non incidunt consequuntur voluptas, suscipit, eos sapiente asperiores sed qui impedit nobis quae obcaecati ducimus ipsa unde temporibus esse facilis soluta eaque! Voluptatum vel ad, voluptas dignissimos tempore assumenda in odit esse eum rem! Ullam magni tempore aliquid. Veritatis culpa odit adipisci, voluptatibus ipsum magnam cum harum excepturi repudiandae.
+    </p>
+    <a href="/guides">View Guides</a>
+    <a href="/">Home</a>
+</div>
+
+<style>
+    .about {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
